a49cbf098beac43c7f794d24d1263d5fbe0d684b
Auclown <auclown@gmail.com>
2020-12-13 23:43:27 +1300

    13th/Dec/2020 - added Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          {/* <Route exact path="/" component={}></Route> */}
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
